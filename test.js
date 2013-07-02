ef3b8d376f58e10ec8a3ad01b0bd441db73f2db1
Gary Katsevman <git@gkatsev.com>
2013-07-02 00:25:35 -0400

    Add releaseObjectGroup

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 var wsurl = process.argv[2] || 'ws://localhost:9222/devtools/page/65705C35-F263-639A-1E0D-E456E1DD4415';
 console.log(wsurl);
 var cd = require('./index.js')(wsurl);
+var objectGroup = '999';
 
 cd.runtime.evaluate({
   expression: 'var foo = {a:5}; foo',
-  objectGroup: '999'
+  objectGroup: objectGroup
 }, function(err, res) {
   if (err) return;
 
@@ -16,6 +17,12 @@ cd.runtime.evaluate({
     if (err) return;
 
     console.log('properties!', res);
+
+    cd.runtime.releaseObjectGroup(objectGroup, function(err, res) {
+      if (err) return;
+
+      console.log('release multiple', res);
+    });
 
     cd.runtime.releaseObject(remoteObjId, function(err, res) {
       if (err) return;
