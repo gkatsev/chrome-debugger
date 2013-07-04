f455197ff68b5d63b4e9d7dd526133898968e008
Gary Katsevman <git@gkatsev.com>
2013-07-04 00:39:35 -0400

    Stub out console methods

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var
   WebSocket = require('./lib/websocket.js'),
-  runtime = require('./lib/runtime.js');
+  runtime = require('./lib/runtime.js'),
+  rconsole = require('./lib/console.js');
 
 module.exports = function(wsurl) {
   var ws = WebSocket(wsurl);
   return {
+    console: rconsole(ws),
     runtime: runtime(ws)
   }
 }
